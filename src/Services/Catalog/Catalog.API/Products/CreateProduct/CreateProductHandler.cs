82fe19e5865f2a16dc8fd88a8a6c3dd30645b034
AsiriZenith <asirisenith@gmail.com>
2025-10-12 00:33:26 +0530

    Add Carter, MediatR, and Mapster support
    - Introduced package references for `Mapster`, `MediatR`, and `Carter` in project files.
    - Added global using directives for cleaner code.
    - Implemented `ICarterModule` in `CreateProductEndpoint` for routing.
    - Updated `CreateProductHandler` to use `ICommandHandler` for command processing.
    - Enhanced `Program.cs` to register services for dependency injection.
    - Updated application URL settings in `launchSettings.json` for development.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,18 +1,28 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
-using MediatR;
-using System.Threading;
-using System.Threading.Tasks;
+namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-    : IRequest<CreateProductResult>;
+    : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //save to database
+
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
