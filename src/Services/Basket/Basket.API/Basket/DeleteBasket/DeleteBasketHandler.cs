74e12897ec63f73f2e1aee908ce2778f86cdd4c2
AsiriZenith <asirisenith@gmail.com>
2025-12-11 20:07:36 +0530

    Basket Microservice Implementation

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
+{
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class DeleteBasketCommandHandler(IBasketRepository repository) : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        var basket = await repository.GetBasket(command.UserName);
+
+        if (basket is null)
+        {
+            throw new BasketNotFoundException(command.UserName);
+        }
+
+        await repository.DeleteBasket(command.UserName, cancellationToken);
+
+        return new DeleteBasketResult(true);
+    }
+}
